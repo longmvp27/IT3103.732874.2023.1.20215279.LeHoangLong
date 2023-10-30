7da2ae630aff9fce05f2fe1fce1a7c16369d65f0
longmvp27 <138133752+longmvp27@users.noreply.github.com>
2023-10-31 01:19:35 +0700

    Add files via upload

---
+++ Lab02/AimsProject/Cart.java
@@ -0,0 +1,50 @@
+package Lab02.AimsProject;
+// Le Hoang Long - 20215279
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered;
+    public Cart() {
+        this.qtyOrdered = 0;
+    }
+    //Method to add dvd
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if(qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered += 1;
+            System.out.println("The disc has been added");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+    //Method to remove dvd
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        for(int i = 0; i < qtyOrdered; i++) {
+            if(itemsOrdered[i] == disc) {
+                itemsOrdered[i] = itemsOrdered[qtyOrdered-1];
+                itemsOrdered[qtyOrdered-1] = null;
+                qtyOrdered -= 1;
+                break;
+            }
+        }
+        System.out.println("The dvd has been removed successfully");
+    }
+    //Method to calculate total cost
+    public float totalCost() {
+        float total = 0.0f;
+        for(int i = 0; i < qtyOrdered; i++) {
+            if(itemsOrdered[i] != null) {
+                total += itemsOrdered[i].getCost();
+            }
+        }
+        return total;
+    }
+    //Method to print items in the cart
+    public void printItems() {
+        for(int i = 0; i < qtyOrdered; i++) {
+            System.out.println(itemsOrdered[i].getTitle());
+        }
+    }
+
+
+}
---
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,73 @@
+package Lab02.AimsProject;
+// Le Hoang Long - 20215279
+public class DigitalVideoDisc {
+    private String title;           //Initialize variable string tile
+    private String category;        //Initialize variable string category
+    private String director;        //Initialize variable string director
+    private int length;             //Initialize variable integer length
+    private float cost;             //Initialize variable float cost
+
+    public String getTitle() {      //Getter of title
+        return title;
+    }
+
+    public void setTitle(String title) {    //Setter of title
+        this.title = title;
+    }
+
+    public String getCategory() {   //Getter of category
+        return category;
+    }
+
+    public void setCategory(String category) {  //Setter of category
+        this.category = category;
+    }
+
+    public String getDirector() {   //Getter of director
+        return director;
+    }
+
+    public void setDirector(String director) {  //Setter of director
+        this.director = director;
+    }
+
+    public int getLength() {       //Getter of length
+        return length;
+    }
+
+    public void setLength(int length) {     //Setter of length
+        this.length = length;
+    }
+
+    public float getCost() {       //Getter of cost
+        return cost;
+    }
+
+    public void setCost(float cost) {   //Setter of cost
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title) { //Constructor by tile
+        this.title = title;
+    }
+    public DigitalVideoDisc(String title, String category, float cost) {    //Constructor by category, title and cost
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) { //Constructor by director, category, title and cost
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {   //Constructor by all attributes
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+}
