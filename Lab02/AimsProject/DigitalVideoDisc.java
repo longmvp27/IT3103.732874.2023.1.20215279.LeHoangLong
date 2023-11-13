c2aa95897a9c1f76847ca3571a93268cd899ae27
longmvp27 <longhoangle999@gmail.com>
2023-11-14 04:58:55 +0700

    main

--- Lab02/AimsProject/DigitalVideoDisc.java
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -1,16 +1,11 @@
 package Lab02.AimsProject;
 // Le Hoang Long - 20215279
 public class DigitalVideoDisc {
-    private int id;
-    private static int nbDigitalVideoDisc = 0;
     private String title;           //Initialize variable string tile
     private String category;        //Initialize variable string category
     private String director;        //Initialize variable string director
     private int length;             //Initialize variable integer length
     private float cost;             //Initialize variable float cost
-    public int getId() {
-        return id;
-    }
     public String getTitle() {      //Getter of title
         return title;
     }
@@ -53,13 +48,11 @@ public class DigitalVideoDisc {
 
     public DigitalVideoDisc(String title) { //Constructor by tile
         this.title = title;
-        this.id = ++nbDigitalVideoDisc;
     }
     public DigitalVideoDisc(String title, String category, float cost) {    //Constructor by category, title and cost
         this.title = title;
         this.category = category;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDisc;
     }
 
     public DigitalVideoDisc(String director, String category, String title, float cost) { //Constructor by director, category, title and cost
@@ -67,7 +60,6 @@ public class DigitalVideoDisc {
         this.category = category;
         this.title = title;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDisc;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {   //Constructor by all attributes
@@ -76,13 +68,6 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
-        this.id = ++nbDigitalVideoDisc;
     }
 
-    public String toString() {
-        return "[" + title + "] - [" + category + "] - [" + director + "] - [" + length + "]: [" + cost + "]";
-    }
-    public boolean isMatch(String title) {
-        return this.title.toLowerCase().contains(title.toLowerCase());
-    }
 }
