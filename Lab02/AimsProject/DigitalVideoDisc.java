27a838e6f7bee1b07d8c662426ecd8ea22f0a30d
longmvp27 <longhoangle999@gmail.com>
2023-11-14 03:24:14 +0700

    class members

--- Lab02/AimsProject/DigitalVideoDisc.java
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -1,11 +1,16 @@
 package Lab02.AimsProject;
 // Le Hoang Long - 20215279
 public class DigitalVideoDisc {
+    private int id;
+    private static int nbDigitalVideoDisc = 0;
     private String title;           //Initialize variable string tile
     private String category;        //Initialize variable string category
     private String director;        //Initialize variable string director
     private int length;             //Initialize variable integer length
     private float cost;             //Initialize variable float cost
+    public int getId() {
+        return id;
+    }
     public String getTitle() {      //Getter of title
         return title;
     }
@@ -48,11 +53,13 @@ public class DigitalVideoDisc {
 
     public DigitalVideoDisc(String title) { //Constructor by tile
         this.title = title;
+        this.id = ++nbDigitalVideoDisc;
     }
     public DigitalVideoDisc(String title, String category, float cost) {    //Constructor by category, title and cost
         this.title = title;
         this.category = category;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDisc;
     }
 
     public DigitalVideoDisc(String director, String category, String title, float cost) { //Constructor by director, category, title and cost
@@ -60,6 +67,7 @@ public class DigitalVideoDisc {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDisc;
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {   //Constructor by all attributes
@@ -68,6 +76,7 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        this.id = ++nbDigitalVideoDisc;
     }
 
 }
